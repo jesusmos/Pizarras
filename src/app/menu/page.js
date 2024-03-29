b29dfbc9843650e8075d06f83a95acf1eddc0dfc
Mosquerajesus1519 <jesusaniliomosqueramartinez@gmail.com>
2024-03-28 20:43:03 -0500

    Commit1
    previa terminación de la interfaz App Pizarras

---
+++ src/app/menu/page.js
@@ -0,0 +1,13 @@
+import ViewMenu from "@/components/custom/menu/ViewMenu";
+
+const Menu = () =>{
+
+    return(
+    <div className="w-full h-screen bg-[rgb(38,38,38)] flex-col">
+       <ViewMenu/>
+        
+
+    </div>
+    );
+}
+export default Menu;
---
+++ src/components/custom/typeDraw/TypeDraw.js
@@ -0,0 +1,14 @@
+const TypeDraw = () =>{
+
+    return(
+        <div className="flex flex-col w-full max-w-sm mx-auto">
+        <div className="text-white text-2xl text-center  ">Tipo de sorteo</div>
+        <div className="w-full mt-5">
+            <button className="rounded h-10 w-full bg-red-700 text-white">Hoy</button>\
+            <button className="rounded h-10 w-full bg-red-700 text-white">Especial</button>
+        </div>
+        </div>
+    
+    );
+}
+export default TypeDraw;
